ef617e6039d7d6f210cb8a89711904a9f41e8112
Cameron Barr <cjbarr1013@gmail.com>
2025-04-11 10:42:43 -0400

    Add basic empty cart page.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,28 +3,34 @@ import styles from '../styles/Cart.module.css';
 import CartCard from './CartCard';
 import CartSummary from './CartSummary';
 import { useOutletContext } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Cart() {
   const { cart, handleCartChange, subtotal } = useOutletContext();
 
   return (
-    <div className={styles.cartContainer}>
-      <div className={styles.cart}>
-        <div className={styles.cardContainer}>
-          {cart.length === 0 ?
-            <p>Your cart is empty.</p>
-          : cart.map((product) => (
-              <CartCard
-                key={product.id}
-                product={product}
-                handleCartChange={handleCartChange}
-              ></CartCard>
-            ))
-          }
+    <>
+      {cart.length === 0 ?
+        <div className={styles.emptyCart}>
+          <h2>Your cart is empty.</h2>
+          <Link to="/store/shop">Go back to store</Link>
         </div>
-        <CartSummary subtotal={subtotal}></CartSummary>
-      </div>
-    </div>
+      : <div className={styles.cartContainer}>
+          <div className={styles.cart}>
+            <div className={styles.cardContainer}>
+              {cart.map((product) => (
+                <CartCard
+                  key={product.id}
+                  product={product}
+                  handleCartChange={handleCartChange}
+                ></CartCard>
+              ))}
+            </div>
+            <CartSummary subtotal={subtotal}></CartSummary>
+          </div>
+        </div>
+      }
+    </>
   );
 }
 
